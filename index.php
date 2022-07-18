43f91d12b056f6905bcd137d6c2b55a3aa7b1c66
ginger <shubham Kanaujiya>
2022-07-18 18:27:40 +0530

    create select dropdown option

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    </head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="card">
+              <div class="card-header">
+                    </div>
+                <div class="card-body">
+                 <form>
+                    <div class="form-group">
+                    <!-- create dropdown country -->
+                        <label for="country">Country</label>
+                        <select class="form-control" id="country-dropdown">
+                            <option value="">Select Country</option>   
+                        </select>
+                    </div>
+                    <!-- create dropdown State -->
+                    <div class="form-group">
+                        <label for="state">State</label>
+                        <select class="form-control" id="state-dropdown">
+                        </select>    
+                        </div>                        
+                        
+                    <!-- create dropdown City -->
+                    <div class="form-group">
+                      <label for="city">City</label>
+                      <select class="form-control" id="city-dropdown"> 
+                      </select>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div> 
+    </div>
+
+</body>
+</html>
